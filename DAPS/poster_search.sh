d26df29dea9e70f66fe25ed80ffecfdc4987744a
Christophe DC <62201948+christophedc0@users.noreply.github.com>
2024-09-11 18:09:58 +0200

    Update poster_search.sh

--- DAPS/poster_search.sh
+++ DAPS/poster_search.sh
@@ -1,14 +1,35 @@
-# Credits: BZ
-# Usage 1: Replace the paths to your preferences!
-# Usage 2: alias poster="/volume1/scripts/poster-search.sh"
-# Usage 3: `poster "title"`
-
 #!/usr/bin/env bash
 
-if [ -z ${1+x} ]; then
-  echo no search string found.
-  echo use the following search syntax: poster \"search string\"
+# Original script by: BZ
+# Usage 1: Replace the paths to your preferences!
+# Usage 2: alias poster="/volume1/scripts/poster-search.sh"
+# Usage 3: `poster "title"`#!/usr/bin/env bash
+# Usage 4: if it does not work, chmod +x poster-search.sh
+
+ASSETS="/volume1/data/videos/metadata"
+
+# Check if no search string is provided
+if [ -z "$*" ]; then
+  echo "No search string found."
+  echo "Use the following search syntax: poster \"search string\""
 else
-  echo "search string: *$1*"
-  cd /volume1/data/posters && find . -type f -iname "*$1*"
+  # Display the search string
+  echo "Search string: $*"
+  
+  # Navigate to the directory containing the files
+  cd $ASSETS || { echo "Directory not found"; exit 1; }
+
+  # Search for files matching the search string with wildcards
+  results=$(find . -type f -iname "*$**")
+
+  # Check if any files were found
+  if [ -z "$results" ]; then
+    echo "No files found matching: $*"
+    echo # extra empty line
+  else
+    # Display the search results and highlight the search term using grep --color
+    echo "$results" | grep --color=always -i "$*"
+    # add an extra empty line after the results
+    echo
+  fi
 fi
